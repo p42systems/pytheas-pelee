f9de5439daafed5fddf21dbfb1556cbba9e951fb
dsartori <doug@sartori.ca>
2025-06-14 11:05:02 -0400

    Update theme.ts
    Improve placeholder colours

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
-    primary: "#cb793a",
-    secondary: "#96bbbb",
+    primary: "#936441",
+    secondary: "#B0CCBA",
     tertiary: "#2b2d42",
     quaternary: "#386641",
     quinary: "#6870CD",
